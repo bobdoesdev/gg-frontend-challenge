9d342839d5ac9cb3197624b0bd8c79e1d12dd822
bobe11 <82974293+bobe11@users.noreply.github.com>
2022-01-28 11:56:08 -0500

    add 4th artboard functionality for purchase order state

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,54 @@
-$( document ).ready(function() {
-    var $radios = $('.checkout__radio');
-     var $cardDetails = $('.checkout__card');
+$(document).ready(function () {
+  var $radios = $(".checkout__radio");
 
-    $radios.click(function(e){
+  var total = $(".checkout__paymentCol option:selected").data("value");
+  $(".checkout__total").text("$" + total);
 
-        console.log(e.target);
-        // if(e.target )
+  $radios.click(function (e) {
+    if ($(this).hasClass("checkout__radio--checked")) {
+      return;
+    } else {
+      //remove all inactive required inputs
+      $(".checkout__payment--showing input").prop("disabled", true);
+      $(".checkout__paymentCol--showing select").prop("disabled", true);
 
-        if($(this).hasClass('checkout__radio--checked')){
-            return;
-        } else{
-            $('.checkout__radio--checked').removeClass('checkout__radio--checked');
-            $(this).addClass('checkout__radio--checked');
+      //removed checked and showing from active divs
+      $(".checkout__radio--checked").removeClass("checkout__radio--checked");
+      $(".checkout__payment--showing").removeClass(
+        "checkout__payment--showing"
+      );
+      $(".checkout__paymentCol--showing").removeClass(
+        "checkout__paymentCol--showing"
+      );
 
-            var $input = $(this).find('input')
-            $input.prop('checked', true);
-            ;
+      //add to newly active one
+      $(this).addClass("checkout__radio--checked");
+      var activeOption = $(this).data("payment-type");
 
-            if($input.attr('id') === 'credit_card'){
-                $cardDetails.addClass('checkout__card--showing');
-                $cardDetails.find('input').attr('required', true);
-            } else{
-                $cardDetails.removeClass('checkout__card--showing');
-                $cardDetails.find('input').attr('required', false);
-            }
-        }
+      $(
+        '.checkout__payment[data-payment-type="' + activeOption + '"]'
+      ).addClass("checkout__payment--showing");
+      $(
+        '.checkout__paymentCol[data-payment-type="' + activeOption + '"]'
+      ).addClass("checkout__paymentCol--showing");
 
+      //require newly active inputs
+      $(".checkout__payment--showing input").prop("disabled", false);
+      $(".checkout__paymentCol--showing select").prop("disabled", false);
 
-    });
+      // //add all active required inputs
+      var $input = $(this).find("input");
+      $input.prop("checked", true);
+    }
+  });
 
+  var $select = $(".checkout__inner--alt select");
 
+  $select.change(function () {
+    var total = $(this).find("option:selected").data("value");
+    $(this)
+      .parents(".checkout__paymentCol")
+      .find(".checkout__total")
+      .text("$" + total);
+  });
 });
